d98bb86017d75cb0a5e5936b197a9e796ca9490f
Goncharoff <nik.ignition.work@gmail.com>
2018-07-23 11:23:56 +0300

    Some minor style fixes

--- app/src/main/java/com/example/ignition/financetracker/Repository/Repository.kt
+++ app/src/main/java/com/example/ignition/financetracker/Repository/Repository.kt
@@ -1,5 +1,6 @@
 package com.example.ignition.financetracker.Repository
 
+import com.example.ignition.financetracker.FinancialOperations.AvailableOperations
 import com.example.ignition.financetracker.FinancialOperations.CurrencyEntity
 import java.math.BigDecimal
 
@@ -9,8 +10,22 @@ class Repository : IRepository {
             CurrencyEntity(BigDecimal.valueOf(300)),
             CurrencyEntity(BigDecimal.valueOf(150)),
             CurrencyEntity(BigDecimal.valueOf(150)),
-            CurrencyEntity(BigDecimal.valueOf(100))
+            CurrencyEntity(BigDecimal.valueOf(100)),
+            CurrencyEntity(BigDecimal.valueOf(150)),
+            CurrencyEntity(BigDecimal.valueOf(150)),
+            CurrencyEntity(BigDecimal.valueOf(150))
+
     )
 
+    //history of operation will be removed/upgradet in the future
+    private val historyOperatrion = arrayListOf<AvailableOperations>(
+            AvailableOperations.INCOME,
+            AvailableOperations.INCOME,
+            AvailableOperations.INCOME,
+            AvailableOperations.OUTCOME,
+            AvailableOperations.OUTCOME,
+            AvailableOperations.OUTCOME
+
+    )
     override fun getData() = data
 }
